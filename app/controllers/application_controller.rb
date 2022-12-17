a1f72364a777be31bc195fbef1c6cd472f28bc8b
kamishubaizumo <84670496+kamishubaizumo@users.noreply.github.com>
2022-12-17 09:56:17 +0000

    19章から

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,20 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
    def after_sign_in_path_for(resource)
-    about_path
+    post_images_path
+    #投稿画像の一覧画面へ。
    end
   
   
     def after_sign_out_path_for(resource)
     about_path
-  end
+    end
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
   
+
 end
